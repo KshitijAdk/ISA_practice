da1b006897af20b58c80a248667cd3bc8c9239b4
Kshitij Adhikari <np03cs4s230250@heraldcollege.edu.np>
2023-07-10 01:14:46 +0545

    Practice codes of ISA module

---
+++ rps.js
@@ -0,0 +1,31 @@
+/**
+ * Computes result of the game. returns victory message if move 1 wins.
+ * @param  {Number}   move1  move 1
+ * @param  {Number}   move2  move 2
+ * @return {String}   result result of the game
+ */
+
+function calcResult(move1, move2) {}
+
+/**
+ * @return {Number}   random number between 0 and 2
+ */
+
+function randomMove() {
+  return Math.floor(Math.random() * 3);
+}
+
+/**
+ * Displays start state of game
+ */
+
+function startGame() {}
+
+/**
+ * Displays end state of game
+ * @param {Event} event event containing information of users input.
+ */
+
+function endGame(event) {}
+
+startGame();
